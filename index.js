85894ef31e11ade62dd91f4a799bee16ace6e3e0
nitza lev <nizinlev@gmail.com>
2022-08-29 19:22:26 +0300

    upload

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from nitzan Lambda at git!'),
+    };
+    return response;
+};
